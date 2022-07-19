a4f3107dd143e5cf8179c2e7e8882cb98d6a9594
Duminda Kuruppu <kuruppu.duminda@gmail.com>
2022-07-19 21:23:08 +0530

    game comeplete

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,25 @@
 import React from "react";
+import Square from "./Square";
+import "../Style.css";
 
-const Board = () => <div>Board</div>
+const style = {
+  border: "2px solid black",
+  borderRadius: "10px",
+  width: "350px",
+  height: "350px",
+  margin: "0 auto",
+  display: "grid",
+  gridTemplate: "repeat(3, 1fr) / repeat(3, 1fr)",
+};
+
+const Board = ({ squares, onClick }) => (
+
+  <div style={style}>
+    {squares.map((square, i) => (
+      <Square key={i} value={square} onClick={() => onClick(i)} />
+    ))}
+  </div>
+
+);
 
 export default Board;
