233224a5f0a545a2033ec0c58e7b67ff6bbd662d
Duminda Kuruppu <kuruppu.duminda@gmail.com>
2022-07-19 16:13:01 +0530

    Scaffolding components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Game from './components/Game';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Duminda</h1>
+        <Game />
       </header>
     </div>
   );
